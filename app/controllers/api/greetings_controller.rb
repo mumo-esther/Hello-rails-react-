bb4fcbff3edb0430edb5b7b509a239c3902adbe1
mumo-esther <mumoesther2021@gmail.com>
2023-09-01 14:43:58 +0300

    Correct app.js location error

--- app/controllers/api/greetings_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::GreetingsController < ApplicationController
-    def random_greeting
-      random_message = Message.order("RANDOM()").first
-      render json: { greeting: random_message.content }
-    end
-  end
-  
